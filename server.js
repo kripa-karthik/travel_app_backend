e062e442648b625686b3fb23f5dc4a787607b1ad
kripa-karthik <kripa1352001@gmail.com>
2024-04-05 11:47:02 +0530

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
+const cors=require('cors');
 
 dotenv.config();
 
@@ -18,10 +19,11 @@ const connectDB=require('./config/dbconfig');
 
 const app=express();
 
+app.use(cors());
 app.use(express.json());
 connectDB();
 
-const PORT=3000;
+const PORT=3500;
 
 app.get("/",(req,res)=>{
     res.send('hello geeks');
